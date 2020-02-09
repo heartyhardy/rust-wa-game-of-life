f68fb8c9df48e803f3e8d2906911d41f7042342f
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-02-09 16:39:11 +0530

    Fixed bugs. Working in text rendering

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,21 @@ mod utils;
 use std::fmt;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
+// If wee_alloc is enabled, it is used as globla allocator
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// Defines Cell state
+// Cell state
 #[wasm_bindgen]
-#[repr(u8)] //Represents each cell as byte
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(u8)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub enum Cell {
     Dead = 0,
     Alive = 1,
 }
 
-// Defines Universe
+// Defome strict Universe. Game of Life will be simulated according to this type
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
@@ -26,59 +25,56 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
-//Translates given row and col to index
+//Implement Universe functions
+#[wasm_bindgen]
 impl Universe {
 
-    // Constructor
-    pub fn new() -> Universe{
-        let width= 64;
-        let height=64;
+    pub fn new() ->Universe{
+        let width = 64;
+        let height = 64;
 
         let cells = (0..width * height)
-        .map(|i| {
-            if i % 2 == 0 && i % 7 == 0{
-                Cell::Alive
-            }else{
-                Cell::Dead
-            }
-        })
-        .collect();
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
 
-        Universe{
+        Universe {
             width,
             height,
-            cells
+            cells,
         }
     }
 
-    // To string
-    pub fn render(&self) ->String{
+    pub fn render(&self) -> String {
         self.to_string()
     }
 
-    //Translates row and column to index
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
 
-    //Gets live neighbours count
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
-        for delta_row in [self.height, 0, 1].iter().cloned() {
-            for delta_col in [self.width, 0, 1].iter().cloned() {
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
                 if delta_row == 0 && delta_col == 0 {
                     continue;
                 }
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let indx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[indx] as u8;
+
+                let nrow = (row + delta_row) % self.height;
+                let ncol = (column + delta_col) % self.width;
+                let idx = self.get_index(nrow, ncol);
+                count += self.cells[idx] as u8
             }
         }
         count
     }
 
-    // On tick get neighbor stats
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -86,9 +82,9 @@ impl Universe {
             for col in 0..self.width {
                 let idx = self.get_index(row, col);
                 let cell = self.cells[idx];
-                let live_count = self.live_neighbor_count(row, col);
+                let live_neighbors = self.live_neighbor_count(row, col);
 
-                let next_cell = match (cell, live_count) {
+                let next_cell = match (cell, live_neighbors) {
                     // Rule 1: Any live cell with fewer than two live neighbours
                     // dies, as if caused by underpopulation.
                     (Cell::Alive, x) if x < 2 => Cell::Dead,
@@ -111,15 +107,14 @@ impl Universe {
     }
 }
 
-// Implement Display for Universe so it can be rendered as text
-impl fmt::Display for Universe{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
-        for line in self.cells.as_slice().chunks(self.width as usize){
-            for &cell in line{
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
                 let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
                 write!(f,"{}",symbol)?;
             }
-            write!(f, "\n")?;
+            write!(f,"{}","\n");
         }
         Ok(())
     }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,16 @@
-import * as wasm from "game-of-life";
+//Import Universe from pkg
+import { Universe } from "game-of-life";
 
-wasm.greet("Charith");
+//Get pre element
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+// Get pre text to render in each tick, call requestAnimationFrame with a tick function
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+  
+    requestAnimationFrame(renderLoop);
+  };
+
+  requestAnimationFrame(renderLoop);
